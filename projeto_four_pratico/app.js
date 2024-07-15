5ebefc0f986edbe552bee04fd1cb787331aa8983
luissilvak <luis.silva@tcema.tc.br>
2024-07-15 14:03:09 -0300

    Modificando projeto four

--- projeto_four_pratico/app.js
+++ projeto_four_pratico/app.js
@@ -1,28 +1,21 @@
 // Projeto AluGames
 
-function alterarStatus() {
-    // Mudar o status de Devolver para Alugar
-    let botao = document.getElementsByClassName('dashboard__item__button dashboard__item__button--return');
-    
+function alterarStatus(num) {
+    // Mudar o status de Alugar para Devolver
+    let num = document.querySelector(`game-${num}`);
+    let botao = num.querySelector('dashboard__item__button'); 
+    let img = num.querySelector('dashboard__item__img');
+
+    let verificarBotao = botao.classList.contains('dashboard__item__button');
+
     // se o botão Devolver for clicado mudar o status Alugar
-    if (botao.classList.contains('dashboard__item__button dashboard__item__button--return')) {
-        botao.classList.remove('dashboard__item__button dashboard__item__button--return');
-        botao.classList.add('dashboard__item__button');
+    if (verificarBotao) {
+        botao.classList.remove('dashboard__item__button--return');
+        botao.innerHTML= ""
     } else {
+        botao.classList.add('dashboard__item__button');
         botao.classList.remove('dashboard__item__button');
-        botao.classList.add('dashboard__item__button dashboard__item__button--return');
     }
-    
-    alterarStatus(1);
 }
-
-// Jogo Monopoly
-
-// Jogo Ticket to Ride
-
-// Jogo Takenoko
-
-
-
 
 
