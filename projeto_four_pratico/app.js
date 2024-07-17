8e31ec2f911fa107cc4c31bcbe28e1f3479c0579
luissilvak <luis.silva@tcema.tc.br>
2024-07-17 12:15:34 -0300

    Adicionando informações

--- projeto_four_pratico/app.js
+++ projeto_four_pratico/app.js
@@ -2,22 +2,23 @@
 
 function alterarStatus(num) {
     // Mudar o status de Alugar para Devolver
-    let num = document.querySelector(`game-${num}`);
-    let botao = num.querySelector('dashboard__item__button'); 
-    let img = num.querySelector('dashboard__item__img');
+    let jogo = document.getElementById(`game-${num}`);
+    
+    
+    let botao = num.querySelector('.dashboard__item__button'); 
+    let img = num.querySelector('.dashboard__item__img');
 
-    let verificarBotao = botao.classList.contains('dashboard__item__button');
+    let verificarBotao = botao.classList.contains('dashboard__item__button--return');
 
-    // se o botão Devolver for clicado mudar o status Alugar
+    // Se o botão Devolver for clicado mudar o status Alugar
     if (verificarBotao) {
-        //botao.classList.remove('dashboard__item__button--return');
-        botao.setAttribute('class', 'dashboard__item__button--return');
-        img.setAttribute('dashboard__item__img--rented');
-        botao.innerHTML = 'Devolver';
-    } else {
-        botao.setAttribute('dashboard__item__button');
-        img.setAttribute('dashboard__item__img');
+        img.classList.remove('dashboard__item__button--rented');
+        botao.classList.remove('dashboard__item__button--return');
         botao.innerHTML = 'Alugar';
+    } else {
+        img.classList.add('dashboard__item__button');
+        botao.classList.add('');
+        botao.innerHTML = 'Devolver';
     }
 }
 
